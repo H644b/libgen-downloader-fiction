1d3fa111e2c25303f4a2358c75f69f2d8163a3e1
H644b <69388525+H644b@users.noreply.github.com>
2025-04-16 01:00:30 -0400

    Update selectors.ts

--- src/api/selectors.ts
+++ src/api/selectors.ts
@@ -4,13 +4,11 @@ export default {
 
   // --- Fiction Selectors ---
   FICTION_RESULTS_TABLE_SELECTOR: "table.catalog tbody",
-  // Selector for the link(s) on the FICTION DETAIL page that lead to the DOWNLOAD page (like books.ms)
-  // It's within the 'Download:' row, inside a 'ul.record_mirrors > li > a'
-  FICTION_DETAIL_DL_PAGE_LINK_SELECTOR: "td.record_mirrors a", // Gets the first link in the download mirrors list
+  // CORRECTED Selector for the link(s) on the FICTION DETAIL page that lead to the DOWNLOAD page
+  // Targets the first link inside the list with class 'record_mirrors'
+  FICTION_DETAIL_DL_PAGE_LINK_SELECTOR: "ul.record_mirrors a", // Corrected selector
 
   // --- Download Page Selectors (Common to Sci-Tech and Fiction Download Pages like books.ms) ---
-  // Selector for the primary "GET" link on the final download page
-  MAIN_DOWNLOAD_URL_SELECTOR: "#download h2 a", // The big GET button link
-  // Selector for the container of alternative (IPFS, TOR) links on the final download page
-  OTHER_DOWNLOAD_URLS_SELECTOR: "#download ul", // The list containing Cloudflare, IPFS.io etc.
+  MAIN_DOWNLOAD_URL_SELECTOR: "#download h2 a",
+  OTHER_DOWNLOAD_URLS_SELECTOR: "#download ul",
 };
